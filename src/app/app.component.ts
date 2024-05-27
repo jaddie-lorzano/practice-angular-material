589a2fcf4f7b302bb9a38a8f4632b34e62135624
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-27 18:07:52 +0800

    initiate repo for practicing template driven form in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tdf-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'practice-angular-material';
+  contact: Contact = {
+    firstname: '',
+    lastname: '',
+  };
+}
+
+interface Contact {
+  firstname: string;
+  lastname: string;
 }
---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tdf-contact-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.scss'
+})
+export class ContactListComponent {
+
+}
