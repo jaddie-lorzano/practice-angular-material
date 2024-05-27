40959fadc7cc1ae0e1301b7dd33bf149a076825a
jaddie-lorzano <jaddielorzano@gmail.com>
2024-05-27 18:21:01 +0800

    update angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'tdf-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'practice-angular-material';
-  contact: Contact = {
-    firstname: '',
-    lastname: '',
-  };
-}
-
-interface Contact {
-  firstname: string;
-  lastname: string;
 }
